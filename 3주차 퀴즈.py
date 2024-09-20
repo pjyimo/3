aff8061e0cf8bc7a817743d7e7744f441fede717
pjyimo <163504531+pjyimo@users.noreply.github.com>
2024-09-20 20:38:33 +0900

    Add files via upload

---
+++ 3주차 퀴즈.py
@@ -0,0 +1,13 @@
+hello = 'Hello, World!'
+print(hello[0:5])
+
+result = (7+3)**2
+print(result)
+
+A = [1, 2, 3, 4, 5]
+result = A[::-1]
+print(result)
+
+fruits = {'apple': 3, 'banana': 5, 'cherry': 2}
+banana_value = fruits['banana']
+print(banana_value)
